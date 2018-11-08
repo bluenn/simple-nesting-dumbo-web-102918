5a9ee912ab317d9edd1442f7e483bcc5ca92e0b3
a lan <bluenowte97@gmail.com>
2018-11-08 14:50:21 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -84,8 +84,9 @@ def adding_matz
 
 
     programmer_hash[:yukihiro_matsumoto] = {}
-    programmer_hash[:yukihiro_matsumoto] = :known_for
-    programmer_hash[:yukihiro_matsumoto] = :languages
+    #programmer_hash[:yukihiro_matsumoto] = :known_for
+    #programmer_hash[:yukihiro_matsumoto] = :languages
+    #can't do that as am writing over instead of adding 
     programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
     programmer_hash[:yukihiro_matsumoto][:languages] = []
     programmer_hash[:yukihiro_matsumoto][:languages][0] = "LISP"
